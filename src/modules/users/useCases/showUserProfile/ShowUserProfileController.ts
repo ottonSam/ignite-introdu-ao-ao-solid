362b92b9b17f7a9ca909062128ee3ce98be0fead
Samuel <ottonsam@gmail.com>
2022-10-16 20:00:13 -0300

    incomplete show user feature

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -3,10 +3,19 @@ import { Request, Response } from "express";
 import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase";
 
 class ShowUserProfileController {
-  constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
+  constructor(private showUserProfileUseCase: ShowUserProfileUseCase) { }
 
   handle(request: Request, response: Response): Response {
     // Complete aqui
+    const { user_id } = request.params;
+
+    const user = this.showUserProfileUseCase.execute({ user_id });
+
+    if (user === undefined) {
+      return response.status(404).json({ error: "mensagem do erro" });
+    }
+    
+    return response.json(user);
   }
 }
 
