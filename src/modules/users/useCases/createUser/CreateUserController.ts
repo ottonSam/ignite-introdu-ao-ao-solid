66d01af16b458dda533a4531c755c5eb22dc552e
Samuel <ottonsam@gmail.com>
2022-10-16 20:17:23 -0300

    Fix #1 controllers using try/catch

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -8,14 +8,12 @@ class CreateUserController {
   handle(request: Request, response: Response): Response {
     // Complete aqui
     const { name, email } = request.body;
-
-    const user = this.createUserUseCase.execute({ name, email });
-
-    if (user === undefined) {
-      return response.status(400).json({ error: "mensagem do erro"});
+    try {
+      const user = this.createUserUseCase.execute({ name, email });
+      return response.status(201).json(user);
+    } catch (err) {
+      return response.status(400).json({ error: err });
     }
-
-    return response.status(201).json(user);
   }
 }
 
