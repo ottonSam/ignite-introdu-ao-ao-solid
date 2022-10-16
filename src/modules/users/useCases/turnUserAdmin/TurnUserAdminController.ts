63ec8d935cac30df35f21f5eda03c49a1862e3e5
Samuel <ottonsam@gmail.com>
2022-10-16 20:00:44 -0300

    incomplete turn user admin feature

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -7,6 +7,15 @@ class TurnUserAdminController {
 
   handle(request: Request, response: Response): Response {
     // Complete aqui
+    const { user_id } = request.params;
+
+    const user = this.turnUserAdminUseCase.execute({ user_id });
+
+    if (user === undefined) {
+      return response.status(404).json({ error: "mensagem do erro" });
+    }
+
+    return response.status(201).json(user);
   }
 }
 
